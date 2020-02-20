7a315f7ec19586f8ccc6f23fe8f09af28f36c156
Alban MOIZAN <alban.moizan@etu.univ-nantes.fr>
2020-02-20 22:11:22 +0100

    patch result

--- src/solver/result.py
+++ src/solver/result.py
@@ -12,23 +12,28 @@ def get_result(obj):
 	libs = []
 	processing = None
 	for i in range(obj.nb_days):
-
-		if processing is None:
-			processing = [obj.next_lib(obj.nb_days - i, rules), i, []]
-			libs.append(processing)
-
-		if processing[0].nb_days < i - processing[1] - 1:
+		print("jour", i)
+		if processing is not None and processing[0].nb_days < (i+1 - processing[1]):
 			processing = None
 
+		if processing is None:
+			temp = obj.next_lib(obj.nb_days - i, rules)
+			if temp is not None:
+				processing = [temp, i, []]
+				libs.append(processing)
+
 		for lib in libs:
-			# print(lib[0].num)
-			if lib[0].nb_days < i - lib[1]:
+			print(lib[0].num, lib[0].nb_days, i, lib[1], processing)
+			if lib[0].nb_days < (i+1 - lib[1] ):
 				tmp = lib[0].next_books(rules)
-				lib[2].append(tmp)
+				print(tmp)
+				if tmp is not None:
+					lib[2].append(tmp)
 	res = [[len(libs)]]
 
 	for lib in libs:
 		lib[2] = sum(lib[2], [])
+		print(lib[0].num, lib[2])
 
 		res.append([lib[0].num, len(lib[2])])
 		res.append(lib[2])
