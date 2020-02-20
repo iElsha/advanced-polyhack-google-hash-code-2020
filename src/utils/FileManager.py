d7f3a170eab92e23b39ed68cb8566232fe07eac8
etienne912 <etienne9912@gmail.com>
2020-02-20 15:30:30 +0100

    + tab indent

--- src/utils/FileManager.py
+++ src/utils/FileManager.py
@@ -8,31 +8,30 @@ __all__ = ['parse', 'output']  # Add in the list every symbols that you want to 
 
 
 def parse(path):
-    with open(path) as file:
-        data = []
-        for row in file.read().split("\n"):
-            object = []
-            for col in row.strip().split(" "):
-                if col.isdigit():
-                    object.append(int(col))
-                elif col.isdecimal():
-                    object.append(float(col))
-                else:
-                    object.append(col)
-            data.append(object)
-        return data
+	with open(path) as file:
+		data = []
+		for row in file.read().split("\n"):
+			object = []
+			for col in row.strip().split(" "):
+				if col.isdigit():
+					object.append(int(col))
+				elif col.isdecimal():
+					object.append(float(col))
+				else:
+					object.append(col)
+			data.append(object)
+		return data
 
 
 def output(path, data):
-    if not os.path.exists('./out'):
-        os.makedirs('./out')
+	if not os.path.exists('./out'):
+		os.makedirs('./out')
 
-    string = ""
-    with open(path, "w+") as out:
-        for row in data:
-            for col in row:
-                string += str(col)
-            string += "\n"
+	string = ""
+	with open(path, "w+") as out:
+		for row in data:
+			for col in row:
+				string += str(col)
+			string += "\n"
 
-        out.write(string)
-
+		out.write(string)
