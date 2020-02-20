3ab0fd861d3ed0de9884d0e1c7e4b3f958fdc605
Alban MOIZAN <alban.moizan@etu.univ-nantes.fr>
2020-02-20 20:31:16 +0100

    get result

--- src/solver/result.py
+++ src/solver/result.py
@@ -5,6 +5,21 @@ __all__ = ['get_result']
 
 
 def get_result(obj):
-	pass
+	libs = []
+	for i in range(obj.nb_days):
+		libs.append([obj.nextLib(), i, []])
+
+		for lib in libs:
+			if lib[0].nb_days < i-lib[1]:
+				lib[2].append(lib[0].nextBook())
+	res = [[len(libs)]]
+	for lib in libs:
+		res.append([lib[0].num, len(lib[2])])
+		res.append(lib[2])
+	return res
+
+
+
+
 
 
